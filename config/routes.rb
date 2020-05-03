b56707e8d270aba654c573366803541e4f868616
Ayachicken <digimonmon.a@gmail.com>
2020-05-03 15:21:53 +0900

    応用課題2

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  get '/home/top' => 'home#top'
+  get '/home/about' => 'home#about'
+  root 'home#top'
+
+end
